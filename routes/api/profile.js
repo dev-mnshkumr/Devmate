9a5c384dd0a7f934ee2b3ac17bc6fef4a202ae6a
dev-mnsh <aahi.manish@gmail.com>
2021-03-25 00:02:05 +0530

    Replaced config.get with dotenv

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const request = require('request');
-const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -310,11 +310,7 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${process.env.GITHUB_CLIENT}&client_secret=${process.env.GITHUB_SECRET}`,
       method: 'GET',
       headers: { 'user-agent': 'node.js' },
     };
