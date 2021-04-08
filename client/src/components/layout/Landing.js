3b59dba1180da1bd6d385bd1b3448130d28ab560
dev-mnshkumr <aahi.manish@gmail.com>
2021-04-08 14:01:04 +0530

    some minor html changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Mates</h1>
+          <h1 className='x-large'>Connect with Developers</h1>
           <p className='lead'>
             Create a developer profile/portfolio, share posts and get help from
             other developers
